70a188c9dc9526e841a39900edb8014a56136368
Dmitry Vassiliev <clawfinger@yandex.ru>
2023-01-06 12:12:40 +0500

    number of 1 bits

---
+++ C++/number_of_1_in_bits/number_of_1_in_bits/number_of_1_in_bits.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/565/
+#include <iostream>
+
+int hammingWeight(uint32_t n) {
+    int res = 0;
+    for (int i = 0; i < 32; i++) {
+        if (n & 1 << i) {
+            res++;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
