baa50b18253eeb55cbea81bd038c0b39609895de
Dmitry Vassiliev <clawfinger@yandex.ru>
2023-02-28 10:29:43 +0500

    fibonacci numbers. more tasks

--- C++/fast_fibonacci/fast_fibonacci/fast_fibonacci.cpp
+++ C++/fast_fibonacci/fast_fibonacci/fast_fibonacci.cpp
@@ -28,12 +28,12 @@ int fastFibonacci(int num) {
     return res;
 }
 
-int FibonacciLastDigit(int num) {
+long long FibonacciLastDigit(long long num) {
     if (num <= 1) {
         return num;
     }
-    int prev = 0;
-    int current = 1;
+    long long prev = 0;
+    long long current = 1;
     for (int i = 2; i <= num; i++) {
         int tmp = prev;
         prev = current;
@@ -42,8 +42,67 @@ int FibonacciLastDigit(int num) {
     return current;
 }
 
+std::vector<long long> getPisanoPeriod(long long m) {
+    std::vector<long long> res;
+
+    res.push_back(0);
+    res.push_back(1);
+
+    int prev = 0;
+    int current = 1;
+    for (;;) {
+        int tmp = prev;
+        prev = current;
+        current = (current + tmp) % m;
+        if (prev == 0 && current == 1) {
+            res.pop_back();
+            return res;
+        }
+        res.push_back(current);
+    }
+
+    return res;
+}
+
+long long get_fibonacci_modulo(long long n, long long m) {
+    if (n <= 1)
+        return n;
+
+    auto period = getPisanoPeriod(m);
+    int periodIndex = n % period.size();
+    return period[periodIndex];
+}
+
+int fibonacci_sum(long long n) {
+    if (n <= 1)
+        return n;
+
+    auto period = getPisanoPeriod(10);
+    int periodIndex = (n + 2) % period.size();
+    int lastDigit = period[periodIndex];
+    if (lastDigit == 0) {
+        return 9;
+    }
+    else {
+        return lastDigit - 1;
+    }
+}
+
+long long get_fibonacci_partial_sum(long long from, long long to) {
+    long long full = FibonacciLastDigit((to + 2) % 60);
+    long long start = FibonacciLastDigit((from -1 + 2) % 60);
+    int res = (full - start + 10) % 10;
+    return res;
+}
+
+int fibonacci_sum_squares_naive(long long n) {
+    int N = FibonacciLastDigit((n) % 60);
+    int NPlusOne = FibonacciLastDigit((n + 1) % 60);
+    return (N * NPlusOne) % 10;
+}
+
 int main()
 {
-    std::cout << FibonacciLastDigit(10);
+    std::cout << get_fibonacci_modulo(10, 2);
 }
 
